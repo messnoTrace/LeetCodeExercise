fb17576c2575bda5bbff1763015393a8fb4de018
notrace <chenyang@ikebang.com>
2019-07-01 15:52:15 +0800

    二叉树的搜索范围和

--- src/com/notrace/Main.java
+++ src/com/notrace/Main.java
@@ -45,10 +45,45 @@ public class Main {
     }
 
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
     /**
-     * @see <a href="https://leetcode-cn.com/problems/binary-number-with-alternating-bits/">交替位二进制数</a>
+     * @see <a href="https://leetcode-cn.com/problems/range-sum-of-bst/">二叉搜索树的范围和</a>
+     * @param root
+     * @param L
+     * @param R
+     * @return
+     */
+    public static int rangeSumBST(TreeNode root, int L, int R) {
+
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        }
+        if (root.val > R) {
+            return rangeSumBST(root.left, L, R);
+        }
+
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right , L, R);
+
+
+    }
+
+
+    /**
      * @param n
      * @return
+     * @see <a href="https://leetcode-cn.com/problems/binary-number-with-alternating-bits/">交替位二进制数</a>
      */
     public static boolean hasAlternatingBits(int n) {
         int temp = n ^ (n >> 1);
